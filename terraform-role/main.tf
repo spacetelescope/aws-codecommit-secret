9bf1b015f84c577213f025d443e667d0981f4aa8
YuviPanda <yuvipanda@gmail.com>
2020-04-15 19:28:20 +0530

    Use terraform to create roles for main module
    To insist on using least-privileged roles from the very
    beginning, terraform itself is used to create roles to be
    used by terraform. The code under `terraform-role` should be
    applied as a role / user with privileges in IAM role creation.
    It will provision a role that'll have just enough privileges
    to perform all the actions in the main repo.

---
+++ terraform-role/main.tf
@@ -0,0 +1,84 @@
+terraform {
+  required_version = ">= 0.12.6"
+}
+
+provider "aws" {
+  version = ">= 2.28.1"
+  region  = var.region
+}
+
+
+resource "aws_iam_policy" "codecommit_secrets_setup_policy" {
+  name = "${var.cluster_name}-secrets-setup"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "codecommit:CreateRepository",
+        "codecommit:DeleteRepository",
+        "codecommit:GetRepository",
+        "codecommit:ListRepositories",
+        "codecommit:ListTagsForResource"
+      ],
+      "Effect": "Allow",
+      "Sid": "1",
+      "Resource": "*"
+    },
+    {
+      "Action": [
+        "kms:CreateKey",
+        "kms:DescribeKey",
+        "kms:GetKeyPolicy",
+        "kms:GetKeyRotationStatus",
+        "kms:ListResourceTags",
+        "kms:ScheduleKeyDeletion"
+      ],
+      "Effect": "Allow",
+      "Sid": "2",
+      "Resource": "*"
+    },
+    {
+      "Action": [
+        "iam:CreateRole",
+        "iam:GetRole",
+        "iam:DeleteRole",
+        "iam:CreatePolicy",
+        "iam:GetPolicy",
+        "iam:DeletePolicy",
+        "iam:AttachRolePolicy",
+        "iam:ListInstanceProfilesForRole"
+      ],
+      "Effect": "Allow",
+      "Sid": "3",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "codecommit_secrets_setup_role" {
+  name = "${var.cluster_name}-secrets-setup-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "AWS": "arn:aws:iam::162808325377:user/yuvipanda"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "codecommit_secrets_setup_attachment" {
+  role       = aws_iam_role.codecommit_secrets_setup_role.name
+  policy_arn = aws_iam_policy.codecommit_secrets_setup_policy.arn
+}
